dc2696f477f643633ed8c95e2348aec6aa394d0c
Elvinas Lukasevicius <ell@open.ch>
2019-04-02 14:29:52 +0200

    Facade initialised

---
+++ src/main/java/Shop/Facade.java
@@ -0,0 +1,27 @@
+package Shop;
+
+public class Facade {
+
+    private Shop shop;
+
+    public Facade(Shop shop) {
+        this.shop = shop;
+    }
+
+    private void buyOneOfEach(String brand, User user) {
+    }
+
+    private void buyWithDelivery(User user) {
+       //
+    }
+
+    private void buyWithInstallation(User user) {
+        //
+    }
+
+    private void buyWithFullService(User user) {
+        //
+    }
+
+
+}
